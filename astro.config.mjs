4d1644373aa063ef24ad515e2882d2236fd677b3
jcamilofarfan <farfan1976@hotmail.com>
2024-05-15 08:46:47 -0500

    add landing

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+import svelte from "@astrojs/svelte";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), sitemap(), svelte()],
+	i18n: {
+    defaultLocale: "en",
+    locales: ["es", "en"],
+		routing: {
+			prefixDefaultLocale: true,
+		}
+  }
+});
---
+++ src/components/Theme.svelte
@@ -0,0 +1,36 @@
+<script>
+	import { onMount } from "svelte";
+
+	let theme = "dark";
+	let languaje = "en";
+	let currentPath = '/'
+	onMount(() => {
+		theme = window.localStorage.getItem("theme") || "light";
+		const pathName = window.location.pathname;
+		let languajePath = pathName.split("/")[1];
+		currentPath = pathName.split("/")[2];
+		languaje = window.localStorage.getItem("languaje") || languajePath;
+		if (languajePath !== languaje) {
+			window.location.href = `/${languaje}/${pagePath}`;
+		}
+	});
+	function handleClickTheme() {
+		theme = theme === "light" ? "dark" : "light";
+		window.localStorage.setItem("theme", theme);
+    document.documentElement.classList.toggle("dark");
+	}
+	function handleClickLanguaje() {
+		languaje = languaje === "es" ? "en" : "es";
+		window.localStorage.setItem("languaje", languaje);
+		window.location.href = `/${languaje}/${currentPath}`;
+	}
+</script>
+
+<div>
+	<button on:click={handleClickTheme}>
+		{theme === "light" ? "🌙" : "🌞"}
+	</button>
+	<button on:click={handleClickLanguaje}>
+		{languaje === "en" ? "🇪🇸" : "🇺🇸"}
+	</button>
+</div>
